125083df2b46af48c26a8963fda2b8af8f185b42
Derzhko <alxDerzhko@yandex.ru>
2019-10-15 22:37:05 +0300

    deleted HelloWorld, renamed variable

--- hw2.5/main.c
+++ hw2.5/main.c
@@ -3,22 +3,22 @@
 #include <stdbool.h>
 
 const int stringMaxLength = 15;
-const int asciiTableCodeLength = 26;
+const int arrayLength = 26;
 
 int main()
 {
     bool isPossible = true;
     char string[stringMaxLength];
     char subString[stringMaxLength];
-    int asciiTableCode[asciiTableCodeLength];
+    int charCounter[arrayLength];
     for (int i = 0; i < stringMaxLength; ++i)
     {
         string[i] = '\0';
         subString[i] = '\0';
     }
-    for (int i = 0; i < asciiTableCodeLength; ++i)
+    for (int i = 0; i < arrayLength; ++i)
     {
-        asciiTableCode[i] = 0;
+        charCounter[i] = 0;
     }
     printf("Insert first string\n");
     scanf("%s", string);
@@ -29,19 +29,14 @@ int main()
 
     for (int i = 0; i < stringLength; ++i)
     {
-        ++asciiTableCode[string[i] - 'a'];
+        ++charCounter[string[i] - 'a'];
     }
 
     for (int i = 0; i < subStringLength; ++i)
     {
-        if (asciiTableCode[subString[i#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}] - 'a'] != 0)
+        if (charCounter[subString[i] - 'a'] != 0)
         {
-            --asciiTableCode[subString[i] - 'a'];
+            --charCounter[subString[i] - 'a'];
         }
         else
         {
