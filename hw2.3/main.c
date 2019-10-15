92e0035f9935a524f1153647d428d1e9b686e1df
Derzhko <alxDerzhko@yandex.ru>
2019-10-16 02:16:24 +0300

    убрал сортировку, сравнение числа с 10000 разбил условие if'а

--- hw2.3/main.c
+++ hw2.3/main.c
@@ -5,14 +5,14 @@
 
 const int numbersLength = 10;
 
-int compare(const void * x1, const void * x2)
+int getDigitByPosition(int number, int position)
 {
-    return ( *(int*)x1 - *(int*)x2 );
+    return (number % (int) pow(10, position)) / (int) pow(10, position - 1);
 }
 
 int main()
 {
-    int try = 0;
+    int try = 1234;
     int cows = 0;
     int bulls = 0;
     int numbers[numbersLength];
@@ -21,21 +21,26 @@ int main()
         numbers[i] = i;
     }
     int value = 0;
-    for (int i = 4; i > 0; )
+    for (int i = 0; i < 4; )
     {
         srand(time(NULL));
-        int helper = rand() % (10 - 4 + i) + (4 - i);
-        if (numbers[helper] != -1)
+        int randomIndex = 0;
+        if (i == 0)
         {
-            value = value + numbers[helper] * (int)pow(10, i - 1);
-            numbers[helper] = -1;
-            qsort(&numbers[4 - i], numbersLength - 4 + i, sizeof(int), compare);
-            --i;
+            randomIndex = rand() % 9 + 1;
+
         }
-    }
-    if (value > 10000)
-    {
-        value -= 1000;
+        else
+        {
+            randomIndex = rand() % (9 - i) + i;
+        }
+        if (numbers[randomIndex] != -1)
+        {
+            value = value * 10 + numbers[randomIndex];
+            numbers[randomIndex] = numbers[i];
+            numbers[i] = -1;
+            ++i;
+        }
     }
     do
     {
@@ -47,22 +52,24 @@ int main()
         {
             for (int i = 1; i <= 4; ++i)
             {
-                int valueNumber = (value % (int) pow(10, i)) / (int) pow(10, i - 1);
+                int valuesDigit = getDigitByPosition(value, i);
                 for (int j = 1; j <= 4; ++j)
                 {
-                    int tryNumber = (try % (int) pow(10, j) / (int) pow(10, j - 1));
-                    if ((valueNumber == tryNumber) && (i == j))
+                    int tryDigit = getDigitByPosition(try, j);
+                    if (valuesDigit == tryDigit)
                     {
-                        ++bulls;
-                    } else if (valueNumber == tryNumber)
+                        if (i == j)
+                        {
+                            ++bulls;
+                        }
+                    } else if (valuesDigit == tryDigit)
                     {
                         ++cows;
                     }
                 }
             }
             printf("cows: %d\n", cows);
-            printf("bulls: %d\t", bulls);
-            printf("(value to check the code: %d)\n", value);
+            printf("bulls: %d\n", bulls);
         }
         else
         {
