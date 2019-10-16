a0d2464ccfa5140213ff9afd522e786f5a554369
Alex <a.derzhko@yandex.ru>
2019-10-16 18:43:25 +0300

    улучшил работу otherSum

--- hw2.1/main.c
+++ hw2.1/main.c
@@ -5,14 +5,17 @@ void otherSum(int *sum, int arrayLength, int value)
 {
     if(sum[value - arrayLength + 1] != 0)
     {
-        sum[value - arrayLength] += sum[value - arrayLength + 1];
-        sum[arrayLength - 1] = 0;
         printf("\n");
         for (int i = 0; i < value; ++i)
         {
             printf("%d ", sum[i]);
         }
+        sum[value - arrayLength] += sum[value - arrayLength + 1];
+        sum[arrayLength - 1] = 0;
         otherSum(sum, arrayLength - 1, value);
+    }
+    else if (sum[0] != value)
+    {
     }
     else
     {
