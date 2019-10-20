32b8ea092f80c7d57119528ec3ec6450fa6857cd
Derzhko <alxDerzhko@yandex.ru>
2019-10-20 20:13:06 +0300

    Добавлены функции size, getValue в List

--- hw4.1/CircularList.c
+++ hw4.1/CircularList.c
@@ -7,48 +7,94 @@
 
 struct ListElement
 {
+    int position;
     ListElement* next;
 };
 
-struct CircularList
+struct List
 {
     ListElement* first;
     ListElement* last;
 };
 
-CircularList* createList()
+List* createList(int size)
 {
-    CircularList* newList = malloc(sizeof(CircularList));
+    List* newList = malloc(sizeof(List));
     newList->first = NULL;
     newList->last = NULL;
+    for (int i = 0; i < size; ++i)
+    {
+        add(newList);
+    }
     return newList;
 }
 
-void add(CircularList* list)
+int size(List* list)
+{
+    int size = 0;
+    ListElement* element = list->first;
+    if (list->first == NULL)
+    {
+        return 0;
+    }
+    for (size = 1; element != list->last; ++size)
+    {
+        element = element->next;
+    }
+    return size;
+}
+
+void add(List* list)
 {
     ListElement* newElement = malloc(sizeof(ListElement));
     if (list->first == NULL)
     {
+        newElement->position = 1;
+        newElement->next = NULL;
         list->first = newElement;
         list->last = list->first;
     }
     else
     {
+        newElement->position = ++list->last->position;
         list->last->next = newElement;
         newElement->next = list->first;
         list->last = newElement;
     }
 }
 
-void delete(int position, CircularList* list)
+void delete(int position, List* list)
 {
-    ListElement* previousElement = list->first;
-    ListElement* deletedElement = NULL;
-    for (int i = 1; i < position - 1; ++i)
+    if (position == 1)
     {
-        previousElement = previousElement->next;
+        list->first = list->first->next;
+        free(list->last->next);
+        list->last->next = list->first;
     }
-    deletedElement = previousElement->next;
-    previousElement->next = deletedElement->next;
-    free(deletedElement);
+    else
+    {
+        ListElement *previousElement = list->first;
+        ListElement *deletedElement = NULL;
+        for (int i = 1; i < position - 1; ++i)
+        {
+            previousElement = previousElement->next;
+        }
+        if (position == size(list))
+        {
+            list->last = previousElement;
+        }
+        deletedElement = previousElement->next;
+        previousElement->next = deletedElement->next;
+        free(deletedElement);
+    }
+}
+
+int getValue(int position, List* list)
+{
+    ListElement* element = list->first;
+    for (int i = 1; i < position; ++i)
+    {
+        element = element->next;
+    }
+    return element->position;
 }
