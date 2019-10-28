74008a4f39523aff8cf58599a3b89cbd7d6ce35e
Derzhko <alxDerzhko@yandex.ru>
2019-10-28 23:48:51 +0300

    изменил название time, заменил цикл while на цикл for

--- hw3.4/main.c
+++ hw3.4/main.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-int howManyActions(int people, int actions, int time)
+int howManyActions(int people, int actions, int minutesAgo)
 {
     int countOfPeople = 0;
-    while (people > 0)
+    for ( ; people > 0; --people)
     {
         int peopleActions = 0;
         scanf("%d", &peopleActions);
         int countOfActions = 0;
-        while (peopleActions > 0)
+        for ( ; peopleActions > 0; --peopleActions)
         {
             int actionsTime = 0;
             scanf("%d", &actionsTime);
-            if (actionsTime <= time)
+            if (actionsTime <= minutesAgo)
             {
                 ++countOfActions;
             }
-            --peopleActions;
         }
         if (countOfActions == actions)
         {
             ++countOfPeople;
         }
-        --people;
     }
     return countOfPeople;
 }
@@ -30,9 +28,9 @@ int howManyActions(int people, int actions, int time)
 int main() {
     int people = 0;
     int actions = 0;
-    int time = 0;
+    int minutesAgo = 0;
     printf("Insert count of people, acts, time\n");
-    scanf("%d %d %d", &people, &actions, &time);
-    printf("%d", howManyActions(people, actions, time));
+    scanf("%d %d %d", &people, &actions, &minutesAgo);
+    printf("%d", howManyActions(people, actions, minutesAgo));
     return 0;
 }
