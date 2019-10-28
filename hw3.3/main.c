9b2b6325af82ec5453f27ae50682477e23f32aa2
Alex <a.derzhko@yandex.ru>
2019-10-28 10:14:32 +0300

    copy from reposytories

--- hw3.3/main.c
+++ hw3.3/main.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+const int arrayMaxLength = 20;
+
+void endShift(int numbers[], int arrayLength)
 {
-    int arrayLength = 0;
-    printf("Insert array length\n");
-    scanf("%d", &arrayLength);
-    int *array = malloc(arrayLength * sizeof(int));
-    printf("Insert value\n");
-    for (int i = 0; i < arrayLength; ++i)
+    int count = 0;
+    for (int i = arrayLength - 1; i >= 0; --i)
     {
-        scanf("%d", &array[i]);
-    }
-    int counter = 0;
-    for (int i = 0; i < arrayLength; ++i)
-    {
-        if (array[i] == 0)
+        if (numbers[i] == 0)
         {
-            array[i] = array[i - counter];
-            array[i - counter] = 0;
+            numbers[i] = numbers[arrayLength - count - 1];
+            numbers[arrayLength - count - 1] = 0;
+            ++count;
         }
     }
-    printf("Final array\n");
+}
+
+int main() {
+    int numbers[arrayMaxLength];
+    for (int i = 0; i < arrayMaxLength; ++i)
+    {
+        numbers[i] = 0;
+    }
+    int arrayLength = 0;
+    printf("Insert length\n");
+    scanf("%d", &arrayLength);
+    printf("Insert your numbers\n");
     for (int i = 0; i < arrayLength; ++i)
     {
-        printf("%d ", &array[i]);
+        scanf("%d", &numbers[i]);
+    }
+
+    endShift(numbers, arrayLength);
+
+    printf("Result array\n");
+    for (int i = 0; i < arrayLength; ++i)
+    {
+        printf("%d ", numbers[i]);
     }
     return 0;
 }
