d843466eb3b282b23813d85e622807ce55ce44ac
Derzhko <alxDerzhko@yandex.ru>
2019-11-18 16:55:08 +0300

    пока не придумал

--- hw2.1/main.c
+++ hw2.1/main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void otherSum(int *sum, int nose, )
+const int maxNumber = 20;
+
+void printTerms(int arrayOfTerms[], int indexOfNose, int indexOfTale, int number)
+{
+}
 
 int main() {
-    int value = 0;
-    printf("Insert your value\n");
-    scanf("%d", &value);
-    int *sum = malloc(value * sizeof(int));
-    for (int i = 0; i < value; ++i)
+    int number = 0;
+    int arrayOfTerms[maxNumber];
+    int indexOfNose = 0;
+    int indexOfTale = 0;
+    for (int i = 0; i < maxNumber; ++i)
     {
-        sum[i] = 1;
+        arrayOfTerms[i] = 0;
     }
-    otherSum(sum, value, value);
+    printf("Insert number\n");
+    scanf("%d", &number);
+    arrayOfTerms[0] = 7;
+    printTerms(arrayOfTerms, indexOfNose, indexOfTale);
     return 0;
 }
