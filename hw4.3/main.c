50b2281a8b828f280d951fb0f72b91cd1e4a832f
Derzhko <alxDerzhko@yandex.ru>
2019-11-18 23:22:35 +0300

    second try

--- hw4.3/main.c
+++ hw4.3/main.c
@@ -12,26 +12,26 @@ void spiralOutput(int array[maxLength][maxLength], int length)
     ++j;
     while (delta <= length - 2)
     {
-        for (int index = j ; j <= index + delta; ++j)
+        for (int index = j; j <= index + delta; ++j)
         {
             printf("%d ", array[i][j]);
         }
         --j;
         ++i;
-        for (int index = i ; i <= index + delta; ++i)
+        for (int index = i; i <= index + delta; ++i)
         {
             printf("%d ", array[i][j]);
         }
         --i;
         --j;
         ++delta;
-        for (int index = j ; j >= index - delta; --j)
+        for (int index = j; j >= index - delta; --j)
         {
             printf("%d ", array[i][j]);
         }
         ++j;
         --i;
-        for (int index = i ; i >= index - delta; --i)
+        for (int index = i; i >= index - delta; --i)
         {
             printf("%d ", array[i][j]);
         }
@@ -48,7 +48,8 @@ void spiralOutput(int array[maxLength][maxLength], int length)
     ++i;
 }
 
-int main() {
+int main()
+{
     int array[maxLength][maxLength];
     int length = 0;
     for (int i = 0; i < maxLength; i++)
@@ -68,18 +69,18 @@ int main() {
             scanf("%d", &array[i][j]);
         }
     }
-    printf("Result array\n");
+    printf("Input array\n");
     for (int i = 0; i < length; ++i)
     {
         for (int j = 0; j < length; ++j)
         {
             if (array[i][j] >= 10)
             {
-                printf("%d ", array[i][j]);
+                printf("%d\t", array[i][j]);
             }
             else
             {
-                printf("%d  ", array[i][j]);
+                printf("%d\t", array[i][j]);
             }
         }
         printf("\n");
