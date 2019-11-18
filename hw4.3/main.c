c59c7c5c1c3bf3e61e04696353e75925e2c8901f
Derzhko <alxDerzhko@yandex.ru>
2019-11-18 23:18:18 +0300

    first try

---
+++ hw4.3/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+const int maxLength = 15;
+
+void spiralOutput(int array[maxLength][maxLength], int length)
+{
+    int delta = 0;
+    int indexOfCenter = length / 2;
+    int i = indexOfCenter;
+    int j = indexOfCenter;
+    printf("%d ", array[i][j]);
+    ++j;
+    while (delta <= length - 2)
+    {
+        for (int index = j ; j <= index + delta; ++j)
+        {
+            printf("%d ", array[i][j]);
+        }
+        --j;
+        ++i;
+        for (int index = i ; i <= index + delta; ++i)
+        {
+            printf("%d ", array[i][j]);
+        }
+        --i;
+        --j;
+        ++delta;
+        for (int index = j ; j >= index - delta; --j)
+        {
+            printf("%d ", array[i][j]);
+        }
+        ++j;
+        --i;
+        for (int index = i ; i >= index - delta; --i)
+        {
+            printf("%d ", array[i][j]);
+        }
+        ++i;
+        ++j;
+        ++delta;
+    }
+    --delta;
+    for (int index = j ; j <= index + delta; ++j)
+    {
+        printf("%d ", array[i][j]);
+    }
+    --j;
+    ++i;
+}
+
+int main() {
+    int array[maxLength][maxLength];
+    int length = 0;
+    for (int i = 0; i < maxLength; i++)
+    {
+        for (int j = 0; j < maxLength; j++)
+        {
+            array[i][j] = 0;
+        }
+    }
+    printf("Insert length of array\n");
+    scanf("%d", &length);
+    printf("Insert value\n");
+    for (int i = 0; i < length; ++i)
+    {
+        for (int j = 0; j < length; ++j)
+        {
+            scanf("%d", &array[i][j]);
+        }
+    }
+    printf("Result array\n");
+    for (int i = 0; i < length; ++i)
+    {
+        for (int j = 0; j < length; ++j)
+        {
+            if (array[i][j] >= 10)
+            {
+                printf("%d ", array[i][j]);
+            }
+            else
+            {
+                printf("%d  ", array[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("Spiral output value\n");
+    spiralOutput(array, length);
+    return 0;
+}
