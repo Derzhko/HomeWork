f87582170a343345108ed85c67f59e88aff1d279
Alex <a.derzhko@yandex.ru>
2019-12-12 01:41:53 +0300

    сделал реализацию через список

--- hw4/hw4.2/main.c
+++ hw4/hw4.2/main.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "list.h"
 #include <string.h>
-#include "list.c"
+#include <stdlib.h>
+
+const int maxLength = 20;
 
 void addToList(List *list)
 {
-    //fclose(file);
-    //file = fopen("../phones.txt", "a+");
-    char name[maxLength];
-    char phone[maxLength];
+    char *name = malloc(sizeof(char) * maxLength);
+    char *phone = malloc(sizeof(char) * maxLength);
+    for (int i = 0; i < maxLength; ++i)
+    {
+        name[i] = '\0';
+        phone[i] = '\0';
+    }
     printf("Insert name\n");
+    fgetc(stdin);
     fgets(name, maxLength, stdin);
     printf("Insert phone\n");
     fgets(phone, maxLength, stdin);
+    name[strlen(name) - 1] = '\0';
+    phone[strlen(phone) - 1] = '\0';
     add(list, name, phone);
-    //fputs(name, file);
-    //fputc('\n', file);
-    //fputs(phone, file);
-    //fputc('\n', file);
 }
 
-void findPhone(FILE *file)
+void findPhone(List* list)
 {
-    fclose(file);
-    file = fopen("../phones.txt", "a+");
-    char name[maxLength];
-    char phone[maxLength];
-    printf("Insert name\n");
-    scanf("%s", name);
-    char helper[maxLength];
-    fgets(helper, maxLength, file);
-    while (strncmp(helper, name, strlen(helper) - 1) != 0 && !feof(file))
+    char *name = malloc(sizeof(char) * maxLength);
+    char *phone;
+    for (int i = 0; i < maxLength; ++i)
     {
-        fgets(phone, maxLength, file);
-        fgets(helper, maxLength, file);
+        name[i] = '\0';
     }
-    if (strncmp(helper, name, strlen(helper) - 1) == 0)
+    printf("Insert name\n");
+    fgetc(stdin);
+    fgets(name, maxLength, stdin);
+    name[strlen(name) - 1] = '\0';
+    phone = findByName(list, name);
+    if (phone != NULL)
     {
-        fgets(phone, maxLength, file);
         printf("%s's phone number is %s\n", name, phone);
     }
     else
@@ -46,27 +48,22 @@ void findPhone(FILE *file)
     }
 }
 
-void findTheName(FILE *file)
+void findName(List* list)
 {
-    fclose(file);
-    file = fopen("../phones.txt", "a+");
-    char name[maxLength];
-    char phone[maxLength];
-    printf("Insert phone\n");
-    scanf("%s", phone);
-    char helper[maxLength];
-    fgets(name, maxLength, file);
-    fgets(helper, maxLength, file);
-    while (strncmp(helper, phone, strlen(helper) - 1) != 0 && !feof(file))
+    char *phone = malloc(sizeof(char) * maxLength);
+    char *name;
+    for (int i = 0; i < maxLength; ++i)
     {
-        fgets(name, maxLength, file);
-        fgets(helper, maxLength, file);
+        phone[i] = '\0';
     }
-    if (strncmp(helper, phone, strlen(helper) - 1) == 0)
+    printf("Insert phone\n");
+    fgetc(stdin);
+    fgets(phone, maxLength, stdin);
+    phone[strlen(phone) - 1] = '\0';
+    name = findByPhone(list, phone);
+    if (name != NULL)
     {
-        char name2[maxLength];
-        strncpy(name2, name, strlen(name) - 1);
-        printf("%s's phone number is %s\n", name2, phone);
+        printf("%s's phone number is %s\n", name, phone);
     }
     else
     {
@@ -78,24 +75,53 @@ int main()
 {
     FILE *file;
     file = fopen("../phones.txt", "a+");
-    char name[maxLength];
-    char phone[maxLength];
-    for (int i = 0; i < maxLength; ++i)
-    {
-        name[i] = '\0';
-        phone[i] = '\0';
-    }
     if (file == NULL)
     {
         printf("File is close");
         return 0;
     }
     List *list = createList();
-    while (fgets(name, maxLength, file) != NULL)
+    fieldListByFile(file, list);
+    while (true)
     {
-        fgets(phone, maxLength, file);
-        add(list, name, phone);
+        int value = 0;
+        printf("0 - exit\n");
+        printf("1 - add an entry (name and phone number)\n");
+        printf("2 - find phone by name\n");
+        printf("3 - find the name on the phone\n");
+        printf("4 - save current data to a file\n");
+        scanf("%d", &value);
+        if (value < 0 && value > 4)
+        {
+            continue;
+        }
+        switch (value)
+        {
+            case 1 :
+            {
+                addToList(list);
+                break;
+            }
+            case 2 :
+            {
+                findPhone(list);
+                break;
+            }
+            case 3 :
+            {
+                findName(list);
+                break;
+            }
+            case 4 :
+            {
+                fieldFileByList(file, list);
+                break;
+            }
+            case 0 :
+            {
+                fclose(file);
+                return 0;
+            }
+        }
     }
-
-    return 0;
 }
