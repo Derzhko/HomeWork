f23a1dc50e7293ece63bbd143dc524f1bdc8906c
Derzhko <alxDerzhko@yandex.ru>
2020-04-28 21:43:57 +0300

    sdpf

--- test4.1/complexNumber.c
+++ test4.1/complexNumber.c
@@ -1,5 +1,62 @@
-//
-// Created by Администратор on 28.04.2020.
-//
-
 #include "complexNumber.h"
+#include <stdlib.h>
+
+struct ComplexNumber
+{
+    int real;
+    int imaginary;
+};
+
+ComplexNumber* createNumber(int real, int imaginary)
+{
+    ComplexNumber* newNumber = malloc(sizeof(ComplexNumber));
+    newNumber->real = real;
+    newNumber->imaginary = imaginary;
+    return newNumber;
+}
+
+int getRealPart(ComplexNumber* number)
+{
+    return number->real;
+}
+
+int getImaginaryPart(ComplexNumber* number)
+{
+    return number->imaginary;
+}
+
+
+ComplexNumber* addition(ComplexNumber* number1, ComplexNumber* number2)
+{
+    ComplexNumber* sum = createNumber(0, 0);
+    sum->real = number1->real + number2->real;
+    sum->imaginary = number1->imaginary + number2->imaginary;
+    return sum;
+}
+
+
+ComplexNumber* subtraction(ComplexNumber* number1, ComplexNumber* number2)
+{
+    ComplexNumber* difference = createNumber(0, 0);
+    difference->real = number1->real - number2->real;
+    difference->imaginary = number1->imaginary - number2->imaginary;
+    return difference;
+}
+
+ComplexNumber* multiplication(ComplexNumber* number1, ComplexNumber* number2)
+{
+    ComplexNumber* result = createNumber(0, 0);
+    result->real = (number1->real) * (number2->real) - (number2->imaginary) * (number2->imaginary);
+    result->imaginary = (number1->real) * (number2->imaginary) + (number1->imaginary * (number2->real));
+    return result;
+}
+
+ComplexNumber* division(ComplexNumber* number1, ComplexNumber* number2)
+{
+    ComplexNumber* result = createNumber(0,0);
+    int modul = (number2->real) * (number2->real) + (number2->imaginary) * (number2->imaginary);
+    result->real = ((number1->real) * (number2->real) + (number2->imaginary) * (number2->imaginary)) / modul;
+    result->imaginary = ((number1->imaginary) * (number2->real) - (number1->real) * (number2->imaginary)) / modul;
+    return result;
+}
+
--- test4.1/complexNumber.h
+++ test4.1/complexNumber.h
@@ -1,8 +1,16 @@
-//
-// Created by Администратор on 28.04.2020.
-//
+#ifndef COMPLEXNUMBER_H
+#define COMPLEXNUMBER_H
 
-#ifndef TEST4_1_COMPLEXNUMBER_H
-#define TEST4_1_COMPLEXNUMBER_H
+typedef struct ComplexNumber ComplexNumber;
 
-#endif //TEST4_1_COMPLEXNUMBER_H
+ComplexNumber* createNumber(int real, int imaginary);
+int getRealPart(ComplexNumber* number);
+int getImaginaryPart(ComplexNumber* number);
+
+ComplexNumber* addition(ComplexNumber* number1, ComplexNumber* number2);
+ComplexNumber* subtraction(ComplexNumber* number1, ComplexNumber* number2);
+ComplexNumber* multiplication(ComplexNumber* number1, ComplexNumber* number2);
+ComplexNumber* division(ComplexNumber* number1, ComplexNumber* number2);
+
+
+#endif //COMPLEXNUMBER_H
